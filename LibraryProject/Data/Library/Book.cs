64c1dbcdacc6fd7d2786b4836dab55ff68c6b7ed
YevhenMartyn <martyn.yevhen@gmail.com>
2024-04-22 20:58:56 +0200

    book properties

---
+++ LibraryProject/Data/Library/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Library
+{
+    internal class Book
+    {
+        private string Title { get; set; }
+        private string Author { get; set; }
+        private string Description{ get; set; }
+
+        public Book(string title, string author, string description) { 
+            Title = title;
+            Author = author;
+            Description = description;
+        }
+       
+    }
+}
