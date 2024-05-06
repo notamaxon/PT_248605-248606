d332039ecf63ce00dd90df8bb1b01bcdde56fcb7
Mkasym Huryn <248605@edu.p.lodz.pl>
2024-05-06 19:47:29 +0200

    EventAbstract intermediate stage

---
+++ LibraryProject/Data/Library/EventAbstract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Library
+{
+    public abstract class EventAbstract
+    {
+       public DateTime EventDate { get; set; }
+       public State State { get; set; }
+       public int Id { get; set; }
+
+       
+
+    }
+}
