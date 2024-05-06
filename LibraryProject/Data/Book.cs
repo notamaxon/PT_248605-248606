86646d0da5489f2136fa461580b71a365f5c0403
YevhenMartyn <martyn.yevhen@gmail.com>
2024-05-06 18:43:23 +0200

    adding book

---
+++ LibraryProject/Data/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Book
+    {
+        public string Title{ get; set; }
+        public string Description { get; set; }
+        public Users.User Author { get; set; }
+        public BookGenres Genre { get; set; }
+
+        public Book()
+        {
+            Genre = BookGenres.none;
+            Title = string.Empty;
+            Description = string.Empty;
+            Author = new Author();
+        }
+    }
+
+    public enum BookGenres {
+        none,
+        horror,
+        mystery,
+        thriller,
+        fantasy,
+        adventure,
+        fiction,
+        romance,
+    }
+}
