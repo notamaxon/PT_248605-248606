a7e965401a800524e7b72670026d94324eb1f18b
Mkasym Huryn <248605@edu.p.lodz.pl>
2024-05-06 18:41:51 +0200

    Users

---
+++ LibraryProject/Data/Users/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Users
+{
+    public class Author : User
+    {     
+        public string Information { get; set; }
+       
+    }
+
+}
